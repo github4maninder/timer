fe3a3e82f2b001b33907194b1b5f272af1bb145f
github4maninder <maninder.email@gmail.com>
2021-11-02 20:51:33 -0400

    add timer1.js file

--- timer1.js
+++ timer1.js
@@ -14,3 +14,11 @@ const createTimer = function(time) {
 for (let arg of args) {
   createTimer(arg);
 }
+
+// TEST 
+// node timer1.js 3 5 7 -1
+
+// EDGE CASES
+// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
